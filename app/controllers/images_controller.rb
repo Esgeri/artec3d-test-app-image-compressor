80927f6c346a42560655ae13d2d84075851f3471
Mirbek Askerov <mirbek.askerov@gmail.com>
2021-11-17 18:38:03 +0600

    Add image file uploader by carrierwave, mini-magick

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,7 +3,7 @@ class ImagesController < ApplicationController
     @image = Image.new(image_params)
 
     if @image.save
-      render json: { status: :ok }
+      render json: { message: 'The image file is successfully uploaded!' }, status: :ok
     else
       render json: { message: @image.errors }, status: :unprocessable_entity
     end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ApplicationRecord
+  mount_uploader :image_file, ImageFileUploader
+
   validates :email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
   validates :image_file, presence: true
 end
